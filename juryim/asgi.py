d3c5eb849b28ec02d160174e96a9d50260178754
BioSocEcoSci <yituerju@gmail.com>
2025-02-27 20:04:16 -0500

    Add ASGI configuration for WebSocket support

--- juryim/asgi.py
+++ juryim/asgi.py
@@ -2,7 +2,7 @@ import os
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-from financial.routing import websocket_urlpatterns
+from user_notifications.routing import websocket_urlpatterns
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'juryim.settings')
 
