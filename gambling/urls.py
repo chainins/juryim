58c8db61a60794615be38165d5090b76921ab0b7
BioSocEcoSci <yituerju@gmail.com>
2025-04-03 21:48:18 -0400

    Fix gambling URL patterns to match existing view functions

--- gambling/urls.py
+++ gambling/urls.py
@@ -4,10 +4,9 @@ from . import views
 app_name = 'gambling'
 
 urlpatterns = [
-    path('', views.game_list, name='game_list'),
-    path('game/<int:game_id>/', views.game_detail, name='game_detail'),
+    path('games/', views.game_list, name='game_list'),
+    path('games/<int:game_id>/', views.place_bet, name='place_bet'),
     path('create/', views.create_game, name='create_game'),
     path('bets/', views.user_bets, name='user_bets'),
     path('api/game/<int:game_id>/bet/', views.place_bet_api, name='place_bet_api'),
-    path('<int:game_id>/bet/', views.place_bet, name='place_bet'),
 ] 
